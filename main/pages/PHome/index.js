0db1f1576e1f69f775f678261054c815ef985f87
Pavel <serpork@ya.ru>
2020-06-05 15:00:15 +0700

    add simple auth form

--- main/pages/PHome/index.js
+++ main/pages/PHome/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {observer, useSession} from 'startupjs'
-import { ScrollView } from 'react-native'
+import { observer, useSession, useQueryDoc } from 'startupjs'
+import { ScrollView, Text } from 'react-native'
 import { TestComponent } from 'components'
 import { AuthForm } from 'main/components'
 import './index.styl'
@@ -8,9 +8,14 @@ import { Content } from '@startupjs/ui'
 
 export default observer(function PHome () {
   const [userId] = useSession('userId')
+  const [user] = useQueryDoc('users', { sessionUserId: userId })
+
   return pug`
     ScrollView.root
       Content
-        AuthForm
+        if user
+          Text tada
+        else
+          AuthForm
   `
 })
