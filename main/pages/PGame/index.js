b6b7d3da1ccb8e25a30c3ee6b4c241a218da74ae
Pavel <serpork@ya.ru>
2020-06-06 17:34:08 +0700

    add game actions

--- main/pages/PGame/index.js
+++ main/pages/PGame/index.js
@@ -2,18 +2,26 @@ import React, { useEffect } from 'react'
 import {$root, observer, useDoc, useQueryDoc, useSession} from 'startupjs'
 import { Text, ScrollView } from 'react-native'
 import qs from 'qs'
-import { Content, Div, H3 } from '@startupjs/ui'
+import { Content, Div, H3, Icon } from '@startupjs/ui'
+import { faScroll, faCut, faCube } from '@fortawesome/free-solid-svg-icons'
 import './index.styl'
+
+const ACTIONS = [
+  {icon: faScroll, type: 'paper'},
+  {icon: faCut, type: 'cut'},
+  {icon: faCube, type: 'stone'},
+]
 
 export default observer(function PGame ({match: {params: {gameId}}}) {
   const [userId] = useSession('userId')
   const [user] = useQueryDoc('users', { sessionUserId: userId })
-  const [game] = useDoc('games', gameId)
+  const [game, $game] = useDoc('games', gameId)
+  const isProfessor = user.id === game.professor
 
   useEffect(() => {
     if(!user || !game) return
 
-    if(user.id !== game.professor) {
+    if(!isProfessor) {
       joinGame(game.professor, user.id)
     }
   }, [])
@@ -22,12 +30,20 @@ export default observer(function PGame ({match: {params: {gameId}}}) {
     await $root.scope('games').join({ gameId, userId })
   }
 
+  async function onActionPress(action) {
+    await $root.scope('games').selectAction({gameId, isProfessor, action})
+  }
+
   return pug`
     Div.root
       if !game.opponent
         H3 Waiting for your opponent
       else
         Content
-          Text.text Lets start
+          Text.text Select your action
+          Div.actions
+            each action in ACTIONS
+              Div.action(onPress=() => onActionPress(action.type) key=action.type)
+                Icon(icon=action.icon size=70)
   `
 })
--- model/GameModel.js
+++ model/GameModel.js
@@ -15,9 +15,28 @@ export default class UserModel extends BaseModel {
 
   async join({gameId, userId}) {
     const $game = this.scope(`games.${gameId}`)
-    await this.root.subscribeAsync($game)
+    await this.root.subscribe($game)
 
     $game.set('opponent', userId)
+    return true
+  }
+
+  async selectAction({gameId, isProfessor, action}) {
+    const $game = this.scope(`games.${gameId}`)
+    await this.root.subscribe($game)
+    const rounds = $game.get('rounds')
+
+    const lastRoundIndex = rounds.length - 1
+    const beforeLastRound = rounds[lastRoundIndex - 1] || []
+    const lastRound = rounds[lastRoundIndex] || []
+    lastRound[isProfessor ? 0 : 1] = action
+    if(lastRound[0] && lastRound[1] || (!beforeLastRound[0] || !beforeLastRound[1])) {
+      rounds[lastRoundIndex] = lastRound
+    } else {
+      rounds.push(lastRound)
+    }
+
+    $game.set('rounds', rounds)
     return true
   }
 }
